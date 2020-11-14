211248b41288bb6ae016cbd62c586d22698d16a5
DESKTOP-1998JVL\Alexis <neon50360@gmail.com>
2020-11-14 14:55:51 +0100

    no message

---
+++ kv/service.go
@@ -0,0 +1,102 @@
+package kv
+
+import (
+	"time"
+
+	"github.com/benbjohnson/clock"
+	"github.com/influxdata/influxdb/v2"
+	"github.com/influxdata/influxdb/v2/rand"
+	"github.com/influxdata/influxdb/v2/resource"
+	"github.com/influxdata/influxdb/v2/resource/noop"
+	"github.com/influxdata/influxdb/v2/snowflake"
+	"go.uber.org/zap"
+)
+
+var (
+	_ influxdb.UserService = (*Service)(nil)
+)
+
+// OpPrefix is the prefix for kv errors.
+const OpPrefix = "kv/"
+
+// Service is the struct that influxdb services are implemented on.
+type Service struct {
+	kv          Store
+	log         *zap.Logger
+	clock       clock.Clock
+	Config      ServiceConfig
+	audit       resource.Logger
+	IDGenerator influxdb.IDGenerator
+
+	// FluxLanguageService is used for parsing flux.
+	// If this is unset, operations that require parsing flux
+	// will fail.
+	FluxLanguageService influxdb.FluxLanguageService
+
+	// special ID generator that never returns bytes with backslash,
+	// comma, or space. Used to support very specific encoding of org &
+	// bucket into the old measurement in storage.
+	OrgIDs    influxdb.IDGenerator
+	BucketIDs influxdb.IDGenerator
+
+	TokenGenerator influxdb.TokenGenerator
+	// TODO(desa:ariel): this should not be embedded
+	influxdb.TimeGenerator
+	Hash Crypt
+
+	variableStore *IndexStore
+
+	urmByUserIndex *Index
+}
+
+// NewService returns an instance of a Service.
+func NewService(log *zap.Logger, kv Store, configs ...ServiceConfig) *Service {
+	s := &Service{
+		log:         log,
+		IDGenerator: snowflake.NewIDGenerator(),
+		// Seed the random number generator with the current time
+		OrgIDs:         rand.NewOrgBucketID(time.Now().UnixNano()),
+		BucketIDs:      rand.NewOrgBucketID(time.Now().UnixNano()),
+		TokenGenerator: rand.NewTokenGenerator(64),
+		Hash:           &Bcrypt{},
+		kv:             kv,
+		audit:          noop.ResourceLogger{},
+		TimeGenerator:  influxdb.RealTimeGenerator{},
+		variableStore:  newVariableStore(),
+		urmByUserIndex: NewIndex(URMByUserIndexMapping, WithIndexReadPathEnabled),
+	}
+
+	if len(configs) > 0 {
+		s.Config = configs[0]
+	}
+
+	if s.Config.SessionLength == 0 {
+		s.Config.SessionLength = influxdb.DefaultSessionLength
+	}
+
+	s.clock = s.Config.Clock
+	if s.clock == nil {
+		s.clock = clock.New()
+	}
+	s.FluxLanguageService = s.Config.FluxLanguageService
+
+	return s
+}
+
+// ServiceConfig allows us to configure Services
+type ServiceConfig struct {
+	SessionLength       time.Duration
+	Clock               clock.Clock
+	FluxLanguageService influxdb.FluxLanguageService
+}
+
+// WithResourceLogger sets the resource audit logger for the service.
+func (s *Service) WithResourceLogger(audit resource.Logger) {
+	s.audit = audit
+}
+
+// WithStore sets kv store for the service.
+// Should only be used in tests for mocking.
+func (s *Service) WithStore(store Store) {
+	s.kv = store
+}
---
+++ label/service_test.go
@@ -0,0 +1,93 @@
+package label_test
+
+import (
+	"context"
+	"errors"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/influxdata/influxdb/v2"
+	"github.com/influxdata/influxdb/v2/bolt"
+	"github.com/influxdata/influxdb/v2/kv"
+	"github.com/influxdata/influxdb/v2/kv/migration/all"
+	"github.com/influxdata/influxdb/v2/label"
+	influxdbtesting "github.com/influxdata/influxdb/v2/testing"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestBoltLabelService(t *testing.T) {
+	influxdbtesting.LabelService(initBoltLabelService, t)
+}
+
+func NewTestBoltStore(t *testing.T) (kv.Store, func(), error) {
+	t.Helper()
+
+	f, err := ioutil.TempFile("", "influxdata-bolt-")
+	if err != nil {
+		return nil, nil, errors.New("unable to open temporary boltdb file")
+	}
+	f.Close()
+
+	path := f.Name()
+	ctx := context.Background()
+	logger := zaptest.NewLogger(t)
+	s := bolt.NewKVStore(logger, path, bolt.WithNoSync)
+	if err := s.Open(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	if err := all.Up(ctx, logger, s); err != nil {
+		return nil, nil, err
+	}
+
+	close := func() {
+		s.Close()
+		os.Remove(path)
+	}
+
+	return s, close, nil
+}
+
+func initBoltLabelService(f influxdbtesting.LabelFields, t *testing.T) (influxdb.LabelService, string, func()) {
+	s, closeBolt, err := NewTestBoltStore(t)
+	if err != nil {
+		t.Fatalf("failed to create new kv store: %v", err)
+	}
+
+	svc, op, closeSvc := initLabelService(s, f, t)
+	return svc, op, func() {
+		closeSvc()
+		closeBolt()
+	}
+}
+
+func initLabelService(s kv.Store, f influxdbtesting.LabelFields, t *testing.T) (influxdb.LabelService, string, func()) {
+	st, err := label.NewStore(s)
+	if err != nil {
+		t.Fatalf("failed to create label store: %v", err)
+	}
+
+	svc := label.NewService(st)
+	ctx := context.Background()
+
+	for _, l := range f.Labels {
+		if err := svc.CreateLabel(ctx, l); err != nil {
+			t.Fatalf("failed to populate labels: %v", err)
+		}
+	}
+
+	for _, m := range f.Mappings {
+		if err := svc.CreateLabelMapping(ctx, m); err != nil {
+			t.Fatalf("failed to populate label mappings: %v", err)
+		}
+	}
+
+	return svc, kv.OpPrefix, func() {
+		for _, l := range f.Labels {
+			if err := svc.DeleteLabel(ctx, l.ID); err != nil {
+				t.Logf("failed to remove label: %v", err)
+			}
+		}
+	}
+}
